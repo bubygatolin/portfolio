7b19f6d709b47dadec6d7b8d87c57814b8cfbb82
Buby Gatolin <bubygat@gmail.com>
2023-04-08 10:54:31 -0300

    number

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import {FaWhatsapp} from 'react-icons/fa';
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <>
-      <a className='mb-2  z-[1000] fixed bottom-0 right-0  bg-green-500 inline-block rounded-full p-3 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg ' href='https://wa.me/5511953974702/'>
+      <a className='mb-2  z-[1000] fixed bottom-0 right-0  bg-green-500 inline-block rounded-full p-3 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg ' href='https://wa.me/551195397402/'>
          <FaWhatsapp
           className=''
           size={40} 
